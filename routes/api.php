93700b8eabf18279ae45404cd7c96bcc79fa312c
RajuBanda <100689689+rajupatelbanda@users.noreply.github.com>
2025-11-10 18:32:30 +0530

    Add product routes to API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
@@ -15,5 +16,6 @@ Route::prefix('v1')->group(function () {
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::apiResource('/categories', CategoryController::class);
+        Route::apiResource('/products', ProductController::class);
     });
 });
